4f16ff7ea52c1d755d5505105fb1a8a4db492aff
Jerry <jerry.wang@xuenn.com>
2019-04-02 16:13:03 +0800

    ngFor loop render

--- APM/src/app/app.component.ts
+++ APM/src/app/app.component.ts
@@ -2,11 +2,11 @@ import { Component } from "@angular/core";
 
 @Component({
   selector:'pm-root',
-  template:`<div><h1>{{ pageTitLe }}</h1>
+  template:`<div><h1>{{ pageTitle }}</h1>
   <pm-products></pm-products>
 </div>
    `
 })
 export class AppComponent{
-  pageTitle:string = 'Default Value';
+  pageTitle:string = 'App Title';
 }
